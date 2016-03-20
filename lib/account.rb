6a23c4a032e2bc25fe59940dcf5918997b2f6e24
Sam Jones <sam@samljones.com>
2016-03-20 12:21:27 -0400

    checksum validity

--- lib/account.rb
+++ lib/account.rb
@@ -7,7 +7,16 @@ class Account
     @acct_number.join
   end
 
+  def checksum
+    weighted = @acct_number.reverse.each_with_index.map {|n, i| n*(i+1)}
+    weighted.reduce(:+)
+  end
+
+  def valid?
+    checksum % 11 == 0
+  end
+  
   def to_s
-    account_number
+    "#{account_number}#{' ERR' unless valid?}"
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,5 +4,33 @@ describe 'Account' do
 
     expect(subject.account_number).to eq('123')
   end
+
+  [
+    {
+      account_number: [3,4,5,8,8,2,8,6,5],
+      valid: true,
+      string: '345882865'
+    },
+    {
+      account_number: [4,5,7,5,0,8,0,0,0],
+      valid: true,
+      string: '457508000'
+    },
+    {
+      account_number: [6,6,4,3,7,1,4,9,5],
+      valid: false,
+      string: '664371495 ERR'
+    }
+  ].each do |test|
+    it "should #{'not' unless test[:valid]} have a valid checksum" do
+      subject = Account.new(test[:account_number])
+      expect(subject.valid?).to eq(test[:valid])
+    end
+
+    it "should represent checksum in to_s" do
+      subject = Account.new(test[:account_number])
+      expect(subject.to_s).to eq(test[:string])
+    end
+  end
 end
 
