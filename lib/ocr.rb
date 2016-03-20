f1af335c60c4aee30be716d5c6ba9d9e46314042
Sam Jones <sam@samljones.com>
2016-03-20 12:24:58 -0400

    illegible characters as ?

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -47,7 +47,7 @@ class OCR
     acct_row = input.split(/\n/).take(3)
     acct_num_blocks = acct_row.map { |line| line.scan(/.{3}/) }
     acct_nums = acct_num_blocks[0].zip(acct_num_blocks[1], acct_num_blocks[2])
-    acct_number = acct_nums.map { |num| NUMBERS.find_index(num) }
+    acct_number = acct_nums.map { |num| NUMBERS.find_index(num) || '?'}
 
     Account.new acct_number
   end
--- spec/ocr_spec.rb
+++ spec/ocr_spec.rb
@@ -13,11 +13,17 @@ describe OCR do
               "  | _| _||_||_ |_   ||_||_|\n"\
               "  ||_  _|  | _||_|  ||_| _|",
       num:    '123456789'
+    },
+    {
+      input:  "    _  _     _  _  _  _  _ \n"\
+              "___ _| _||_||_ |_   ||_||_|\n"\
+              "||||_  _|  | _||_|  ||_| _|",
+      num:    '?23456789'
     }
   ].each do |test|
     it "should produce an account number for #{test[:num]}" do
       account = subject.read_acct_number test[:input]
-      expect(account.to_s).to eq(test[:num])
+      expect(account.account_number).to eq(test[:num])
     end
   end
 end
