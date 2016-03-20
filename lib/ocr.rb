88ae1cbb5bc1be15c8ea407c9976346c8a258ba3
Sam Jones <sam@samljones.com>
2016-03-20 11:27:51 -0400

    reading account number line

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -1,5 +1,50 @@
 class OCR
-  def read_line(input)
-    return '000000000'
+  @@numbers = [
+    [' _ ',
+     '| |',
+     '|_|'],
+
+    ['   ',
+     '  |',
+     '  |'],
+
+    [' _ ',
+     ' _|',
+     '|_ '],
+
+    [' _ ',
+     ' _|',
+     ' _|'],
+
+    ['   ',
+     '|_|',
+     '  |'],
+
+    [' _ ',
+     '|_ ',
+     ' _|'],
+
+    [' _ ',
+     '|_ ',
+     '|_|'],
+
+    [' _ ',
+     '  |',
+     '  |'],
+
+    [' _ ',
+     '|_|',
+     '|_|'],
+
+    [' _ ',
+     '|_|',
+     ' _|'],
+  ]
+
+  def read_acct_number(input)
+    acct_row = input.split(/\n/).take(3)
+    acct_num_blocks = acct_row.map { |line| line.scan(/.{3}/) }
+    acct_nums = acct_num_blocks[0].zip(acct_num_blocks[1], acct_num_blocks[2])
+    acct_nums.map { |num| @@numbers.find_index(num) }.join
   end
 end
--- spec/ocr_spec.rb
+++ spec/ocr_spec.rb
@@ -2,13 +2,21 @@ require 'ocr'
 
 describe OCR do
   [
-    input:  " _  _  _  _  _  _  _  _  _ \n"\
-            "| || || || || || || || || |\n"\
-            "|_||_||_||_||_||_||_||_||_|\n",
-    num:    '000000000'
+    {
+      input:  " _  _  _  _  _  _  _  _  _ \n"\
+              "| || || || || || || || || |\n"\
+              "|_||_||_||_||_||_||_||_||_|",
+      num:    '000000000'
+    },
+    {
+      input:  "    _  _     _  _  _  _  _ \n"\
+              "  | _| _||_||_ |_   ||_||_|\n"\
+              "  ||_  _|  | _||_|  ||_| _|",
+      num:    '123456789'
+    }
   ].each do |test|
     it "should produce an account number for #{test[:num]}" do
-      expect(subject.read_line test[:input]).to eq(test[:num])
+      expect(subject.read_acct_number test[:input]).to eq(test[:num])
     end
   end
 end
