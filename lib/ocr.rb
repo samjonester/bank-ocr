0d6f9b3c79c792b4a8b737abb3c0f1ea0aa6ceef
Sam Jones <sam@samljones.com>
2016-03-21 16:34:16 -0400

    finding corrections

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'account'
 
 class OCR
@@ -43,7 +44,7 @@ class OCR
      ' _|'],
   ]
 
-  def read_acct_number(input)
+  def read_account_number(input)
     acct_row = input.split(/\n/).take(3)
     chunks_of_three = acct_row.map { |line| line.scan(/.{3}/) }
     acct_nums = chunks_of_three[0].zip(chunks_of_three[1], chunks_of_three[2])
@@ -51,4 +52,26 @@ class OCR
 
     Account.new acct_number
   end
+
+  def account_corrections(input)
+    [].tap do |acc|
+      (0..input.length).each do |i|
+        acc.concat valid_substitutions(input, i) unless input[i] == "\n"
+      end
+    end.flatten
+  end
+ 
+  SUBSTITUTIONS = [' ', '|', '_']
+  def substitution_chars(char)
+    SUBSTITUTIONS.reject{|s| s == char}
+  end
+
+  def valid_substitutions(input, i)
+    substitution_chars(input[i]).map do |sub_option|
+      test_input = input.dup
+      test_input[i] = sub_option
+      read_account_number(test_input)
+    end.select(&:valid_checksum?)
+  end
+
 end
--- spec/ocr_spec.rb
+++ spec/ocr_spec.rb
@@ -1,29 +1,43 @@
 require 'ocr'
 
 describe OCR do
-  [
-    {
-      input:  " _  _  _  _  _  _  _  _  _ \n"\
-              "| || || || || || || || || |\n"\
-              "|_||_||_||_||_||_||_||_||_|",
-      num:    '000000000'
-    },
-    {
-      input:  "    _  _     _  _  _  _  _ \n"\
-              "  | _| _||_||_ |_   ||_||_|\n"\
-              "  ||_  _|  | _||_|  ||_| _|",
-      num:    '123456789'
-    },
-    {
-      input:  "    _  _     _  _  _  _  _ \n"\
-              "___ _| _||_||_ |_   ||_||_|\n"\
-              "||||_  _|  | _||_|  ||_| _|",
-      num:    '?23456789'
-    }
-  ].each do |test|
-    it "should produce an account number for #{test[:num]}" do
-      account = subject.read_acct_number test[:input]
-      expect(account.account_number).to eq(test[:num])
+  describe 'reading account number' do
+    [
+      {
+        input:  " _  _  _  _  _  _  _  _  _ \n"\
+                "| || || || || || || || || |\n"\
+                "|_||_||_||_||_||_||_||_||_|",
+        num:    '000000000'
+      },
+      {
+        input:  "    _  _     _  _  _  _  _ \n"\
+                "  | _| _||_||_ |_   ||_||_|\n"\
+                "  ||_  _|  | _||_|  ||_| _|",
+        num:    '123456789'
+      },
+      {
+        input:  "    _  _     _  _  _  _  _ \n"\
+                "___ _| _||_||_ |_   ||_||_|\n"\
+                "||||_  _|  | _||_|  ||_| _|",
+        num:    '?23456789'
+      }
+    ].each do |test|
+      it "should produce an account number for #{test[:num]}" do
+        account = subject.read_account_number test[:input]
+        expect(account.account_number).to eq(test[:num])
+      end
+    end
+  end
+
+  describe 'finding corrections' do
+    let(:input) { " _  _  _  _  _  _  _  _  _ \n"\
+                  "|_||_||_||_||_||_||_||_||_|\n"\
+                  "|_||_||_||_||_||_||_||_||_|" }
+    let(:corrections) {[ '888886888', '888888880', '888888988' ]}
+
+
+    it 'should produce a list of corrected account numbers' do
+      expect(subject.account_corrections(input).map(&:account_number)).to eq(corrections)
     end
   end
 end
