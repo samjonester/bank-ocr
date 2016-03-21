cedddff2a4d394f18bcf89bda0b27b69b5b604ec
Sam Jones <sam@samljones.com>
2016-03-21 11:27:43 -0400

    renaming blocks to chunks in OCR parsing

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -45,8 +45,8 @@ class OCR
 
   def read_acct_number(input)
     acct_row = input.split(/\n/).take(3)
-    acct_num_blocks = acct_row.map { |line| line.scan(/.{3}/) }
-    acct_nums = acct_num_blocks[0].zip(acct_num_blocks[1], acct_num_blocks[2])
+    chunks_of_three = acct_row.map { |line| line.scan(/.{3}/) }
+    acct_nums = chunks_of_three[0].zip(chunks_of_three[1], chunks_of_three[2])
     acct_number = acct_nums.map { |num| NUMBERS.find_index(num) || '?'}
 
     Account.new acct_number
