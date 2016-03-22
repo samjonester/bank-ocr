2ab656962ff3fefa47580ec1febcf87be7f11259
Sam Jones <sam@genomoncology.com>
2016-03-22 15:18:46 -0400

    formatting ambiguous accounts

--- lib/ocr/account_formatter.rb
+++ lib/ocr/account_formatter.rb
@@ -4,16 +4,22 @@ class OCR
       @account = account
     end
 
-    def formatted_s(account_number)
-      "#{account_number.join}#{illegible_s(account_number) || error_s(account_number)}"
+    def formatted_s(account_number, alternatives = [])
+      "#{account_str(account_number)}#{illegible_s(account_number) || error_s(account_number)}#{alternatives_str(alternatives)}"
     end
 
     private
+    def account_str(account_number)
+      account_number.join
+    end
     def illegible_s(account_number)
       ' ILL' unless @account.legible?(account_number)
     end
     def error_s(account_number)
       ' ERR' unless @account.valid_checksum?(account_number)
+    end
+    def alternatives_str(alternatives)
+      " AMB #{alternatives.map{|alt| account_str(alt)}.join(' ')}" unless alternatives.empty?
     end
   end
 end
--- spec/ocr/account_formatter_spec.rb
+++ spec/ocr/account_formatter_spec.rb
@@ -27,4 +27,17 @@ describe OCR::AccountFormatter do
 
     expect(subject.formatted_s(account_number)).to eq("#{account_number_str} ILL")
   end
+ 
+  let(:correction_1) { [4,5,6] }
+  let(:correction_1_str) { '456' }
+
+  let(:correction_2) { [7,8,9] }
+  let(:correction_2_str) { '789' }
+  it "should print an ambigious account" do
+    expect(account).to receive(:legible?).with(account_number).and_return(true)
+    expect(account).to receive(:valid_checksum?).with(account_number).and_return(true)
+
+    expect(subject.formatted_s(account_number, [correction_1, correction_2])).to eq("#{account_number_str} AMB #{correction_1_str} #{correction_2_str}")
+  end
+
 end
